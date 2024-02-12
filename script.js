a8b3c93d1641705166e05b11896f276060c97e0b
inclined4X <jezemiahsam48@gmail.com>
2024-02-12 21:28:50 +0000

    used for each for display the buttons clicked for user choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-  
+//getting the IDs of the span elements
+const computerChoice = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+
+//userChoice
+let userChoice;
+
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+  })
+);
